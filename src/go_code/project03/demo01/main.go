b725a49b59d9de7ba0f5be5273a528a65ac17462
dengwenj <927561237@qq.com>
2022-05-18 18:03:29 +0800

    变量的细节2

--- src/go_code/project03/demo01/main.go
+++ src/go_code/project03/demo01/main.go
@@ -11,12 +11,18 @@ func main() {
 	/*
 		变量表示内存中的一个存储区域
 		该区域有自己的名称（变量名）和类型（数据类型）
+		变量如果没有赋初始值，会有默认值，int 默认值为 0 string 默认值为空串。小数默认值为 0
+		同一个作用域下不能重名
 	*/
+	var hh string
 	var num int = 1
 	// 一次声明多个变量的方式
 	var num1, num2, num3 = 11, 22, 33
 	// := 方式声明
 	name := "dengwj"
+	hh = "ww"
+	// 重新赋值
+	name = "dwj"
 
 	//也可以
 	var (
@@ -25,6 +31,6 @@ func main() {
 		sex1  = sex
 	)
 
-	fmt.Println(num, num1, num2, num3, name)
+	fmt.Println(num, num1, num2, num3, name, hh)
 	fmt.Println(name1, age1, sex1)
 }
