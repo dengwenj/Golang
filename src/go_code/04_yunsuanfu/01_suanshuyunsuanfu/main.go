0516eeca7a3167360971735619efe8c882c0628e
dengwenj <927561237@qq.com>
2022-05-19 23:12:36 +0800

    算数运算符基本使用

---
+++ src/go_code/04_yunsuanfu/01_suanshuyunsuanfu/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 如果运算的数都是整数，那么除后，去掉小数部分，保留整数部分
+	fmt.Println(10 / 4) // 2
+	var n1 float32 = 10 / 4
+	fmt.Println(n1) // 2
+
+	// 如果希望保留小数部分，则需要有浮点数参与运算
+	var n2 float32 = 10 / 4.0
+	fmt.Println(n2) // 2.5
+
+	// a % b = a - a / b * b
+	fmt.Println(10 % 3)   // 1
+	fmt.Println(-10 % 3)  // -1
+	fmt.Println(-10 % -3) // -1
+	fmt.Println(10 % -3)  // 1
+}
