f327005e2cdcb29fedaf8024919767358ce3c536
dengwenj <927561237@qq.com>
2022-05-22 16:51:20 +0800

    键盘输入语句

--- src/go_code/04_yunsuanfu/main.go
+++ src/go_code/04_yunsuanfu/main.go
@@ -51,4 +51,26 @@ func main() {
 	fmt.Println(c, d)
 	//c, d = d, c
 	//fmt.Println(c, d)
+
+	var name string
+	var age byte
+	var sal float32
+	var isPass bool
+
+	//fmt.Println("请输入姓名")
+	//fmt.Scanln(&name)
+	//
+	//fmt.Println("请输入年龄")
+	//fmt.Scanln(&age)
+	//
+	//fmt.Println("请输入薪水")
+	//fmt.Scanln(&sal)
+	//
+	//fmt.Println("是否通过")
+	//fmt.Scanln(&isPass)
+
+	//fmt.Printf("姓名%v\n 年龄%v\n 薪水%v\n 是否通过%v\n", name, age, sal, isPass)
+	fmt.Println("请输入姓名、年龄、薪水、是否通过")
+	fmt.Scanf("%s %d %f %t", &name, &age, &sal, &isPass)
+	fmt.Printf("姓名%v\n 年龄%v\n 薪水%v\n 是否通过%v\n", name, age, sal, isPass)
 }
