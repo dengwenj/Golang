d41628aa432bec57192d8217f976f511e65bdeb6
邓文杰 <927561237@qq.com>
2023-03-24 23:35:51 +0800

    测试

--- src/go_ww/user/dwwwj.go
+++ src/go_ww/user/dwwwj.go
@@ -1,5 +1,13 @@
 package user
 
+import "fmt"
+
 func Hello() string {
+	fmt.Printf("打印1: %v\n", test())
+	fmt.Printf("打印2: %v\n", test1())
 	return "dwwwj"
+}
+
+func test() int {
+	return 0
 }
---
+++ src/go_ww/user/ww.go
@@ -0,0 +1,5 @@
+package user
+
+func test1() int {
+	return 1
+}
